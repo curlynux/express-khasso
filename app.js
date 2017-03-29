796894c857de0cdef51abc05ec14ecd7921f5f20
curlynux <smpunchack@gmail.com>
2017-03-29 19:10:05 +0200

    back to previous nodemailer config

--- app.js
+++ app.js
@@ -40,6 +40,44 @@ app.use('/contact', contact);
 app.use('/a-propos', a_propos);
 app.use('/header', header)
 
+app.post('/contact', (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: 'outlook',
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587,
+    tls: {
+       ciphers:'SSLv3'
+   },
+    auth: {
+        user: 'ibrahim.samory@outlook.fr',
+        pass: 'veerbatim87'
+    },
+
+});
+  var sender = req.body.email;
+  var sujet = req.body.sujet;
+  var texte = req.body.message;
+
+  var mailOptions = {
+    from: sender,
+    to: 'ibrahim.samory@outlook.fr',
+    subject: sujet,
+    text: texte
+    html: '<p>le formulaire marche'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.error(error);
+    } else {
+      console.log('Message %s envoyer: %s', info.messageId, info.response);
+    }
+  });
+
+  res.send('req recieved');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
